aadcf9c18bb6510121b43641885da63ebd1b53d9
lsh <hack4569@naver.com>
2025-08-12 07:30:45 +0900

    @springbootTest, @TestConfiguration을 사용하여 테스트

--- src/test/java/tobyspring/splearn/application/provided/MemberRegisterTest.java
+++ src/test/java/tobyspring/splearn/application/provided/MemberRegisterTest.java
@@ -1,86 +1,42 @@
 package tobyspring.splearn.application.provided;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.test.util.ReflectionTestUtils;
-import tobyspring.splearn.application.MemberService;
-import tobyspring.splearn.application.required.EmailSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestConstructor;
+import tobyspring.splearn.SplearnTestConfigration;
 import tobyspring.splearn.application.required.MemberRepository;
-import tobyspring.splearn.domain.Email;
+import tobyspring.splearn.domain.DuplicateEmailException;
 import tobyspring.splearn.domain.Member;
 import tobyspring.splearn.domain.MemberFixture;
 import tobyspring.splearn.domain.MemberStatus;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class MemberRegisterTest {
-    @Test
-    void registerTestStub() {
-        MemberRegister register = new MemberService(
-                new MemberRepositoryStub(), new EmailSenderStub(), MemberFixture.createPasswordEncoder()
-        );
+@SpringBootTest
+@Transactional
+public class MemberRegisterTest {
 
-        Member member = register.register(MemberFixture.createMemberRegisterRequest());
-
-        assertThat(member.getId()).isNotNull();
-    }
-    @Test
-    void registerTestMock() {
-        EmailSenderMock emailSender = new EmailSenderMock();
-        MemberRegister register = new MemberService(
-                new MemberRepositoryStub(), emailSender, MemberFixture.createPasswordEncoder()
-        );
-
-        Member member = register.register(MemberFixture.createMemberRegisterRequest());
-
-        assertThat(member.getId()).isNotNull();
-        assertThat(emailSender.tos).hasSize(1);
-        assertThat(emailSender.tos.getFirst()).isEqualTo(member.getEmail());
-    }
+    @Autowired
+    MemberRegister memberRegister;
 
     @Test
-    void registerTestMockito() {
-        EmailSender emailSender = Mockito.mock(EmailSender.class);
-
-        MemberRegister register = new MemberService(
-                new MemberRepositoryStub(), emailSender, MemberFixture.createPasswordEncoder()
-        );
-
-        Member member = register.register(MemberFixture.createMemberRegisterRequest());
+    void register() {
+        Member member = memberRegister.register(MemberFixture.createMemberRegisterRequest());
 
         assertThat(member.getId()).isNotNull();
         assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
 
-        Mockito.verify(emailSender).send( eq(member.getEmail()), any(), any());
     }
 
-    static class MemberRepositoryStub implements MemberRepository {
-        @Override
-        public Member save(Member member) {
-            ReflectionTestUtils.setField(member, "id", 1L);
-            return member;
-        }
-    }
+    @Test
+    void duplicateEmailFail() {
+        Member member = memberRegister.register(MemberFixture.createMemberRegisterRequest());
 
-    static class EmailSenderStub implements EmailSender {
-        @Override
-        public void send(Email email, String subject, String body) {
-
-        }
-    }
-
-    static class EmailSenderMock implements EmailSender {
-        List<Email> tos = new ArrayList<>();
-
-        @Override
-        public void send(Email email, String subject, String body) {
-            tos.add(email);
-        }
+        assertThatThrownBy( () -> memberRegister.register(MemberFixture.createMemberRegisterRequest()))
+                .isInstanceOf(DuplicateEmailException.class);
     }
 }
